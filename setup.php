51a854c13fcdcbf48126877ea502e5f8a2007f6b
mendel <mendel.saldua@macvad.com>
2017-05-30 14:00:24 +1200

    Initial commit

---
+++ setup.php
@@ -0,0 +1,11 @@
+
+===============
+Learn Git
+===============
+
+1. Signup to Github with mendelcoop@gmail.com
+2. Create new ci - project folder
+3. Using command to open the project folder
+4. $ git init
+5. 
+
